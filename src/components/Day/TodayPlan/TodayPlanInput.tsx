7fde6ba6b9ced8925c582483bd3f2d79c935ac7b
yoorimChoi <yulim3839@naver.com>
2022-07-14 01:03:33 +0900

    feat: ✨ 하루 요약 컴포넌트 및 기능

---
+++ src/components/Day/TodayPlan/TodayPlanInput.tsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
+function TodayPlanInput() {
+  const [todayPlan, setTodayPlan] = useState<string>();
+  const [timer, setTimer] = useState<NodeJS.Timeout | number>(0);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+    setTodayPlan(todayPlan);
+    if (timer) {
+      clearTimeout(timer);
+    }
+    const newTimer = setTimeout(async () => {
+      try {
+        //서버와 api 통신
+        console.log('저장');
+      } catch (e) {
+        console.error('error', e);
+      }
+    }, 2500);
+    setTimer(newTimer);
+  };
+  return (
+    <StyledTodayPlanInput.Root>
+      <StyledTodayPlanInput.Input
+        placeholder="오늘 하루 계획 및 다짐을 입력해주세요."
+        onChange={handleChange}
+      />
+      <StyledTodayPlanInput.Hr />
+    </StyledTodayPlanInput.Root>
+  );
+}
+
+export default TodayPlanInput;
+
+const StyledTodayPlanInput = {
+  Root: styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 0.4rem;
+    justify-content: left;
+  `,
+  Input: styled.input`
+    margin-top: 0.4rem;
+    border: none;
+    padding: 0;
+    font-size: 1.2rem;
+    &::placeholder {
+      color: #d8dbde;
+    }
+  `,
+  Hr: styled.hr`
+    margin: 0;
+    border: 0.1rem solid;
+    padding: 0;
+    width: 47.3rem;
+    color: #d8dbde;
+  `,
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
+import TodayPlan from 'src/components/Day/TodayPlan';
 import { theme } from 'src/styles/theme';
 import styled from 'styled-components';
 
 function Home() {
-  return <Styled.Root>안녕얘들아</Styled.Root>;
+  return (
+    <Styled.Root>
+      <TodayPlan />
+    </Styled.Root>
+  );
 }
 
 export default Home;
