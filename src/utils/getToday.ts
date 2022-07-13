dd27c36c7448d839c681cd48e0e4854dfe65b004
1 <wrasf175@naver.com>
2022-07-14 03:09:58 +0900

    fix:  🔨 atoms 수정

--- src/utils/getToday.ts
+++ src/utils/getToday.ts
@@ -3,6 +3,8 @@ export const todayTime = () => {
   const todayYear = today.getFullYear();
   const todayMonth = today.getMonth() + 1;
   const todayDate = today.getDate();
+  const week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+  const dayOfWeek = week[today.getDay()];
 
-  return todayYear + '0' + todayMonth + todayDate;
+  return todayYear + '0' + todayMonth + todayDate + dayOfWeek;
 };
