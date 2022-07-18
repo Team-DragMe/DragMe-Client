ef1c54ada494651d974ff8ccdd2516178f26176a
1 <wrasf175@naver.com>
2022-07-19 02:50:38 +0900

    feat: ✨ 모달 및 클릭시 콘솔찍는 기능 추가

--- src/pages/day/[date].tsx
+++ src/pages/day/[date].tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 
 import NavBar from '../../components/common/NavBar';
+import DayPlanSettingModal from '../../components/Day/DayPlanSettingModal';
 
 function Day() {
   return (
     <Styled.Root>
       <NavBar />
-      데이페이지입니다.
+      <DayPlanSettingModal />
     </Styled.Root>
   );
 }
