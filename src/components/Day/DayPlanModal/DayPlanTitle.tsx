328ab2977cd1d6fa47808a847f1e9b2c1ccab613
yoorimChoi <yulim3839@naver.com>
2022-07-19 23:33:52 +0900

    feat: ✨ 상위계획 제목 수정 기능 제작

--- src/components/Day/DayPlanModal/DayPlanTitle.tsx
+++ src/components/Day/DayPlanModal/DayPlanTitle.tsx
@@ -1,8 +1,29 @@
+import React, { useState } from 'react';
 import { theme } from 'src/styles/theme';
 import styled from 'styled-components';
 
-function DayPlanTitle() {
-  return <Styled.TitleInput placeholder="제목 및 시간 추가(12자 이내 입력)" />;
+interface DayPlanTitleProps {
+  handleChangeTitle: (newTitle: string) => void;
+  title: string;
+}
+
+function DayPlanTitle(props: DayPlanTitleProps) {
+  const { title, handleChangeTitle } = props;
+  const [newTitle, setNewTitle] = useState(title);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target instanceof HTMLInputElement) {
+      handleChangeTitle(newTitle);
+      setNewTitle(e.target.value);
+    }
+  };
+
+  return (
+    <Styled.TitleInput
+      placeholder="제목 및 시간 추가(12자 이내 입력)"
+      onChange={handleChange}
+      value={newTitle}
+    />
+  );
 }
 
 export default DayPlanTitle;
@@ -12,6 +33,9 @@ const Styled = {
     width: 17.1rem;
     height: 1.8rem;
     border: none;
+    &:focus {
+      outline: none;
+    }
     &::placeholder {
       color: ${theme.colors.letter_grey};
       font-size: 1.2rem;
