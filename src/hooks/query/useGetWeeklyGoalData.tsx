92799ec6a9b2edba45c72516688bd72ffc22caec
sunny <huiseon37@gmail.com>
2022-07-21 04:54:44 +0900

    feat: ✨ 주간목표 쿼리 훅 수정

--- src/hooks/query/useGetWeeklyGoalData.tsx
+++ src/hooks/query/useGetWeeklyGoalData.tsx
@@ -2,8 +2,9 @@ import { useQuery } from 'react-query';
 import { getWeeklyGoalData } from 'src/lib/api/weekApi';
 import { StartDateQuery } from 'src/types/week';
 
-const useGetWeeklyGoalData = ({ startDate }: StartDateQuery) =>
-  useQuery(
+const useGetWeeklyGoalData = ({ startDate }: StartDateQuery) => {
+  console.log('>>들어오는 startDate', startDate);
+  return useQuery(
     ['weeklyGoal', startDate],
     async () =>
       getWeeklyGoalData({
@@ -13,9 +14,10 @@ const useGetWeeklyGoalData = ({ startDate }: StartDateQuery) =>
       select: (data) => data.data,
       keepPreviousData: true,
       useErrorBoundary: true,
-      retry: 0,
+      retry: 3,
       suspense: false,
     },
   );
+};
 
 export default useGetWeeklyGoalData;
