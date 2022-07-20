629a45b242174d279213269e041b27fe503d33f2
1 <wrasf175@naver.com>
2022-07-20 23:36:10 +0900

    chore : ⚙️ 콘솔로그 테스트 코드 삭제

--- src/utils/getWeek.ts
+++ src/utils/getWeek.ts
@@ -4,21 +4,16 @@ export const getCurrentWeek = (count: number) => {
   let mondayStandard = countDay.getDay() - 1;
   const year = countDay.getFullYear();
   const month = ('0' + (countDay.getMonth() + 1)).slice(-2);
+
   if (mondayStandard === -1) {
     mondayStandard = 6;
   }
-
-  console.log('요일은>>>>>>>', countDay);
-  console.log('일요일 기준 요일 판단>>>>>>>', countDay.getDay());
-  console.log('월요일 기준 요일 판단>>>>>>>', mondayStandard);
 
   const monday = countDay.getTime() - 86400000 * mondayStandard;
 
   countDay.setTime(monday);
 
   const result = [year + '-' + month + '-' + countDay.toString().slice(8, 10)];
-
-  console.log('반복문 돌리기 전>>>>>>>>>', result);
 
   for (let i = 1; i < 7; i++) {
     countDay.setTime(countDay.getTime() + 86400000);
