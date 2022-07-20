be4d2e4b295410af30ff48a46f58e4eba7dd8e6a
sunny <huiseon37@gmail.com>
2022-07-21 07:10:23 +0900

    feat: ✨ 하루노트 조회 api 연결

--- src/components/Day/TodayNote/TodayNoteSection.tsx
+++ src/components/Day/TodayNote/TodayNoteSection.tsx
@@ -1,13 +1,20 @@
+import { useRecoilValue } from 'recoil';
+import useGetTodayNoteData from 'src/hooks/query/useGetTodayNoteData';
+import { dayInfo } from 'src/states';
 import styled from 'styled-components';
 
 import TodayPlan from '../TodayPlan';
 import TodayNote from './TodayNote';
 
 function TodayNoteSection() {
+  const date = useRecoilValue(dayInfo).slice(0, 10);
+  const { data } = useGetTodayNoteData({ date });
+  const todayNoteInfo = data?.data;
+
   return (
     <Styled.Root>
-      <TodayPlan />
-      <TodayNote />
+      <TodayPlan emoji={todayNoteInfo?.emoji || ''} note={todayNoteInfo?.dailyGoal || ''} />
+      <TodayNote memo={todayNoteInfo?.memo || ''} />
     </Styled.Root>
   );
 }
