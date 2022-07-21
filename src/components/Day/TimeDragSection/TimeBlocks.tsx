d5a1d90d3887c8f5a0a11547bc629b436e7c9523
sunny <huiseon37@gmail.com>
2022-07-21 23:39:36 +0900

    feat: ✨ 시간 블럭 로직 수정

--- src/components/Day/TimeDragSection/TimeBlocks.tsx
+++ src/components/Day/TimeDragSection/TimeBlocks.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import usePostScheduleTime from 'src/hooks/query/usePostScheduleTime';
 import useDragBlock from 'src/hooks/useDragBlock';
 import { getTimeArray } from 'src/utils/dateUtil';
 import styled from 'styled-components';
@@ -17,10 +18,12 @@ interface TimeBlocksProps {
 
 function TimeBlocks({ blocksId }: TimeBlocksProps) {
   const { timeArr } = getTimeArray();
+  const { mutate: postScheduleTime } = usePostScheduleTime();
   const [isDragging, setIsDragging] = useState(false);
   const [isUsed] = useState(false);
   const [startBlock, setStartBlock] = useState('');
   const [endBlock, setEndBlock] = useState('');
+  const [isClickMakeBlock, setIsClickMakeBlock] = useState(false);
   //임시 더미데이터 from 서버
   const testData = { id: 'tempId1', isUsed: true, timeBlockNumbers: [82, 83, 84, 85, 86] };
 
@@ -39,12 +42,33 @@ function TimeBlocks({ blocksId }: TimeBlocksProps) {
     const blockList = [];
     const start = parseInt(startBlock);
     const end = parseInt(endBlock);
-    console.log(startBlock, endBlock);
     for (let i = start < end ? start : end; i <= (start < end ? end : start); i++) {
       blockList.push(i);
     }
 
-    console.log(blockList);
+    //start, end로 요청 추가, 삭제 분기처리
+    if (start < end) {
+      console.log('생성 배열', blockList);
+      postScheduleTime({
+        scheduleId: '62d59f0cae6800e410c70572',
+        isUsed: false,
+        timeBlockNumbers: blockList,
+      });
+    } else if (start > end) {
+      //삭제요청
+      console.log('삭제 배열', blockList);
+    } else if (start === end) {
+      if (isClickMakeBlock) {
+        console.log('클릭 하나 생성', blockList);
+        postScheduleTime({
+          scheduleId: '62d59f0cae6800e410c70572',
+          isUsed: false,
+          timeBlockNumbers: blockList,
+        });
+      } else {
+        console.log('클릭 하나 삭제', blockList);
+      }
+    }
   };
 
   //서버에서 가져온 데이터 블럭 생성
@@ -66,6 +90,7 @@ function TimeBlocks({ blocksId }: TimeBlocksProps) {
           startBlock={startBlock}
           endBlock={endBlock}
           isDraged={isDraged(el)}
+          setIsClickMakeBlock={setIsClickMakeBlock}
         />
       ))}
     </Styled.Root>
