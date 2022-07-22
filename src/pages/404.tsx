0080ed349c0e979254810af9cee637e905b771d4
1 <wrasf175@naver.com>
2022-07-22 19:24:09 +0900

    feat: ✨ 404페이지 제작, favicon 재교체

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,54 @@
+import Error from 'public/assets/404PageError.svg';
+import styled from 'styled-components';
+
 export default function Custom404() {
-  return <h1>404 Error - Page Not Found</h1>;
+  return (
+    <Styled.Root>
+      <Styled.Error404 />
+      <Styled.Title>서버 응답을 받지 못했습니다.</Styled.Title>
+      <Styled.Text>
+        네트워크 상태를 받지 못했습니다.
+        <br /> 문제가 계속되면 잠시 후 다시 시도해주세요.
+      </Styled.Text>
+    </Styled.Root>
+  );
 }
+const Styled = {
+  Root: styled.div`
+    position: relative;
+    background: #f5f5f5;
+    width: 100%;
+    height: 84rem;
+  `,
+  Error404: styled(Error)`
+    position: absolute;
+    width: 839.37px;
+    height: 449px;
+    left: 353px;
+    top: 150px;
+  `,
+  Title: styled.p`
+    position: absolute;
+    width: 318px;
+    height: 42px;
+    left: 554px;
+    top: 614px;
+    font-weight: 700;
+    font-size: 28px;
+    line-height: 150%;
+    text-align: center;
+    text-transform: uppercase;
+  `,
+  Text: styled.p`
+    position: absolute;
+    width: 341px;
+    height: 52px;
+    left: 543px;
+    top: 664px;
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 130%;
+    text-align: center;
+    text-transform: uppercase;
+  `,
+};
