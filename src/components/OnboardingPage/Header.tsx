a0c7623ba31aeb734819f3b1c48d0347444488be
1 <wrasf175@naver.com>
2023-03-13 02:16:26 +0900

    css:🎨  온보딩페이지 헤더 불필요한 코드 수정

--- src/components/OnboardingPage/Header.tsx
+++ src/components/OnboardingPage/Header.tsx
@@ -6,12 +6,8 @@ import styled from 'styled-components';
 function OnBoardingPage() {
   return (
     <Styled.Root>
-      <Styled.LogoContainer>
-        <Image src={Logo} alt="온보딩 로고" width={'1440'} height={'900'} />
-      </Styled.LogoContainer>
-      <Styled.GradientArea>
-        <Styled.SubText>Find one&#39;s own direction</Styled.SubText>
-      </Styled.GradientArea>
+      <Image src={Logo} alt="main logo" width={1440} height={900} />
+      <Styled.SubText>Find one&#39;s own direction</Styled.SubText>
     </Styled.Root>
   );
 }
@@ -19,16 +15,10 @@ export default OnBoardingPage;
 const Styled = {
   Root: styled.div`
     position: relative;
-    width: 100%;
-    height: 84rem;
+    width: 144rem;
+    height: 90rem;
     display: flex;
     flex-direction: column;
-  `,
-  LogoContainer: styled.div`
-    z-index: -1;
-    position: absolute;
-    width: 100%;
-    bottom: 1.2rem;
   `,
   SubText: styled.p`
     position: absolute;
@@ -44,13 +34,5 @@ const Styled = {
     line-height: 2rem;
 
     color: rgba(20, 54, 151, 0.2);
-  `,
-  GradientArea: styled.div`
-    z-index: 2;
-    position: absolute;
-    width: 100%;
-    height: 84rem;
-
-    background: linear-gradient(0deg, #dadff7 0%, #dadff7 0.01%, rgba(218, 223, 247, 0) 59.94%);
   `,
 };
