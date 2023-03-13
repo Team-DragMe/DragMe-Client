38f136acbcf0f2b8868392f64a92bed4216d7b52
1 <wrasf175@naver.com>
2023-03-13 21:34:35 +0900

    css:🎨 온보딩 페이지 헤더 텍스트 추가

--- src/components/OnboardingPage/Header.tsx
+++ src/components/OnboardingPage/Header.tsx
@@ -8,6 +8,10 @@ function OnBoardingPage() {
     <Styled.Root>
       <Image src={Logo} alt="main logo" width={1440} height={900} />
       <Styled.SubText>Find one&#39;s own direction</Styled.SubText>
+      <Styled.Description>
+        느슨하지만 단단한 플랜 서비스로
+        <br /> 나다움의 방향성을 찾아가다.
+      </Styled.Description>
     </Styled.Root>
   );
 }
@@ -29,5 +33,16 @@ const Styled = {
     font-size: 2.4rem;
 
     color: rgba(20, 54, 151, 0.2);
+  `,
+  Description: styled.p`
+    position: absolute;
+    left: 4rem;
+    top: 9.7rem;
+    font-style: normal;
+
+    font-weight: bold;
+    font-size: 3.2rem;
+    line-height: 150%;
+    letter-spacing: -0.05em;
   `,
 };
